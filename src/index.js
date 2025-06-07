e34bc6fccaa5b0dc0e1e07a660e2a6fb67b75230
Matias <lit.io30303@gmail.com>
2025-06-07 15:38:08 -0400

    Creando el auth de suppabase

---
+++ src/index.js
@@ -0,0 +1,7 @@
+export {default as App} from './App';
+export * from './context/AuthContext';
+export * from './main';
+export * from './styles/breackpoints';
+export * from './styles/variables';
+export * from './supabase/supabase.config';
+export * from './utils/dataEstatica';
